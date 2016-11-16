693bb5bd49a5bf0bf48535360eac966de1cfb5d4
Andrew Ripka <aripka@pivotal.io>
2016-11-15 21:31:24 -0600

    change class names

--- src/main/java/io/pivotal/workshop/WorkshopConfigApplication.java
+++ src/main/java/io/pivotal/workshop/WorkshopConfigApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class FortuneConfigApplication {
+public class WorkshopConfigApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FortuneConfigApplication.class, args);
+		SpringApplication.run(WorkshopConfigApplication.class, args);
 	}
 }
--- src/test/java/io/pivotal/workshop/WorkshopConfigApplicationTests.java
+++ src/test/java/io/pivotal/workshop/WorkshopConfigApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class FortuneConfigApplicationTests {
+public class WorkshopConfigApplicationTests {
 
 	@Test
 	public void contextLoads() {
